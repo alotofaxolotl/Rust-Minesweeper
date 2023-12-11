7fa86b7318a47968c2246b6ff9a6b071c52a53f4
Jeremy <jeremygrice02@gmail.com>
2023-12-11 15:15:31 +1100

    Implement minefield data structure * Add Cell struct * Implement builder functions for Cell and the field

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use rand::{Rng, rngs::ThreadRng};
+
+#[derive(Debug)]
+struct Cell {
+    clear: bool,
+    flagged: bool,
+    mine: bool
+}
+
+fn build_cell(mine_chance: &f32, rng: &mut ThreadRng) -> Cell {
+    Cell {
+        clear: false,
+        flagged: false,
+        mine: (rng.gen::<f32>() < *mine_chance) as bool
+    }
+}
+
+fn build_field(size: usize, mine_chance: f32) -> Vec<Vec<Cell>> {
+    let mut rng = rand::thread_rng();
+    let mut field: Vec<Vec<Cell>> = Vec::new();
+    for _ in 0..size {
+        field.push(vec![build_cell(&mine_chance, &mut rng)]);
+    }
+    field
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut field = build_field(10, 0.1);
 }
